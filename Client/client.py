0c8a9680db9b0fc6c7dfcb9e92eae543b9df79d3
ret7020 <ret7020@gmail.com>
2022-08-04 23:14:58 +0300

    Auth architecture created

--- Client/client.py
+++ Client/client.py
@@ -1,11 +1,22 @@
 import socket
+import json
+import hashlib
+
+user_id = int(input("User id:"))
+user_pass = input("User pass:")
+user_pass_hash = hashlib.sha256(user_pass.encode('utf-8')).hexdigest()
+print(user_pass_hash)
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
 sock.connect(('localhost', 5000))
-sock.send(bytes('Hello, world', encoding='UTF-8')) 
-while True:
-    data = sock.recv(1024) 
-    print(data)
 
+
+# Auth
+auth_dict = {"cmd": 0, "user_id": 0, "user_pass": user_pass_hash}
+sock.send(json.dums(auth_dict).encode("utf-8")) 
+while True:
+    data = sock.recv(1024)
     
+
+
 sock.close()
--- Server/server.py
+++ Server/server.py
@@ -1,5 +1,7 @@
 import socket
-
+import json
+import hashlib
+import sqlite3
 
 class Server:
     def __init__(self, listen_from, port):
@@ -10,20 +12,46 @@ class Server:
 
 
 class DB:
-    def __init__(self):
-        pass
+    def __init__(self, db_path):
+        self.db_path = db_path
+        self.connection = sqlite3.connect(db_path)
+        self.cursor = con.cursor()
 
-    def auth_client(self):
-        pass
+    def auth_client(self, user_id, user_pass_hash):
+        data = self.cursor.execute("SELECT * FROM `users` WHERE `id` = 1 AND `password` = :pass_hash", {"pass_hash": user_pass_hash}) # 1160130875fda0812c99c5e3f1a03516471a6370c4f97129b221938eb4763e63
+        print(data)
 
-class Client:
-    def __init__(self, conn):
+class User:
+    def __init__(self, conn, user_id, sended_hash, nick=None, token=None, authed=False):
         self.conn = conn
+        self.user_id = user_id
+        self.sended_hash = sended_hash
+        self.nick = nick
+        self.token = token
+        self.authed = authed
+    
+    def listen_thread(self):
+        while True:
+            data = self.conn.recv(2048)
+            data = data.decode("utf-8")
+            try:
+                data = json.loads(data)
+                if not self.authed:
+                    if data["cmd"] == 0:
+                        pass
+                    else:
+                        self.conn.close()
+                        self.__del__()
+
+            except:
+                pass
+    def __del__(self):
+        pass
 
 if __name__ == "__main__":
-    '''server = Server("", 5000)
+    server = Server("", 5000)
     while True:
         conn, addr = server.accept()
         print(addr)
         print("[LOG] New client")
-    '''
+    
